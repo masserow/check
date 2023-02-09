0ee72483dc7a9b1ddf8ee414537b2b977358503f
Sergey Masserow <masserow@mail.ru>
2023-02-09 15:56:06 +0300

    src

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         int check = 100;// Начальный счет
-        int refill = 1500;// Сумма пополнения
+        int refill = 1000;// Сумма пополнения
+        int bonus;
         if (refill >= 1000) {
-            int bonus = refill / 100;// Количество бонусных рублей
+            bonus = refill / 100;// Количество бонусных рублей
+        } else {
+            bonus = 0;
+        }
         int endCeck = check + refill + bonus;// Итоговый счет
         System.out.println((endCeck) + "Итоговый счет");
         System.out.println((bonus) + "Количество бонусных рублей");
-        }
+
     }
 }
